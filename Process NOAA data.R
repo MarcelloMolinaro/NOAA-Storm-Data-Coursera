3eb1858dbfe6b94b889f28de85db307c372a9bf4
marcellomm48 <marcelloridesabike@gmail.com>
2020-04-23 00:28:21 -0700

    Finished the cleaning phase...mostly, It's a little lazy with the removing of the duplicates... Ready for analysis!

--- Process NOAA data.R
+++ Process NOAA data.R
@@ -22,54 +22,70 @@ repdata2 <- subset(repdata, select = c("STATE__","STATE", "EVTYPE", "BGN_DATE", 
 #Subsets data to only post 1995 data (when the valuable data began)
 repdata2 <- subset(repdata2, year(strptime(repdata$BGN_DATE, "%m/%d/%Y %H:%M:%S")) > 1995)
 
-#This modifies variable types
-#NA's introcuded here
+#This modifies variable types; NA's introcuded here
 repdata2[, c("STATE__", "LENGTH", "WIDTH", "MAG", "FATALITIES","INJURIES", "PROPDMG", 
              "CROPDMG")] <- lapply(repdata2[c("STATE__", "LENGTH", "WIDTH", "MAG", "FATALITIES",
                                               "INJURIES", "PROPDMG", "CROPDMG")], function(x) as.numeric(as.character(x)))
-#adds POSIXlt data types for startand end dates
+#adds POSIXlt data types for start and end dates
 repdata2 <- cbind(repdata2, 
       NewStartDate= strptime(repdata2$BGN_DATE, format = "%m/%d/%Y %H:%M:%S"),
       NewEndDate= strptime(repdata2$END_DATE, format = "%m/%d/%Y %H:%M:%S")
       )
+#treats Damage exponents and creates new columns
+repdata2 <- cbind(repdata2, 
+                  cumPropDam = case_when(
+                                  repdata2$PROPDMGEXP == "B" ~ repdata2$PROPDMG * 1000000000,
+                                  repdata2$PROPDMGEXP == "M" ~ repdata2$PROPDMG * 1000000,
+                                  repdata2$PROPDMGEXP == "K" ~ repdata2$PROPDMG * 1000
+                  ),
+                  cumCropDam = case_when(
+                                  repdata2$CROPDMGEXP == "B" ~ repdata2$CROPDMG * 1000000000,
+                                  repdata2$CROPDMGEXP == "M" ~ repdata2$CROPDMG * 1000000,
+                                  repdata2$CROPDMGEXP == "K" ~ repdata2$CROPDMG * 1000  
+                  )
+            )
 
-#There is a bETTER way to do this...
-amatch(c("poop","winter", "winter m"), c("winter mix", "poop"), maxDist = 4, method = 'dl') or use default 'osa'
+#Converts every event type to lowercase to consolidate types
+repdata2$EVTYPE <- tolower(repdata2$EVTYPE)
+
+#This is required for easier event cleaning, might re-run below
+events <- repdata2 %>% 
+  group_by(EVTYPE) %>% 
+  summarise(instances = n(), totFat = sum(FATALITIES), totInjur = sum(INJURIES), totPropDam = sum(cumPropDam), totCropDam = sum(cumCropDam))
+events <- arrange(events, -totFat)
 
 
+#GO CLEAN YOUR DATA! COME BACK HERE WHEN IT IS CLEAN!
 
 
 #The following Code Summarizes the data by different groups
 states <- repdata2 %>% 
-    group_by(EVTYPE, STATE) %>% 
-    summarise(instances = n(), totFat = sum(FATALITIES), totInjur = sum(INJURIES), totPropDam = sum(PROPDMG), totCropDam = sum(CROPDMG))
+    group_by(lookup, STATE) %>% 
+    summarise(instances = n(), totFat = sum(FATALITIES), totInjur = sum(INJURIES), totPropDam = sum(cumPropDam), totCropDam = sum(cumCropDam))
 
 events <- repdata2 %>% 
-    group_by(EVTYPE) %>% 
-    summarise(instances = n(), totFat = sum(FATALITIES), totInjur = sum(INJURIES), totPropDam = sum(PROPDMG), totCropDam = sum(CROPDMG))
+    group_by(lookup) %>% 
+    summarise(instances = n(), totFat = sum(FATALITIES), totInjur = sum(INJURIES), totPropDam = sum(cumPropDam), totCropDam = sum(cumCropDam))
 events <- arrange(events, -totFat)
 
 years <- repdata2 %>% 
     group_by(Year = year(NewStartDate)) %>%
-    summarise(instances = n(), totFat = sum(FATALITIES), totInjur = sum(INJURIES), totPropDam = sum(PROPDMG), totCropDam = sum(CROPDMG))
+    summarise(instances = n(), totFat = sum(FATALITIES), totInjur = sum(INJURIES), totPropDam = sum(cumPropDam), totCropDam = sum(cumCropDam))
 
 yearsEvent <- repdata2 %>% 
-  group_by(Year = year(NewStartDate), EVTYPE) %>%
-  summarise(instances = n(), totFat = sum(FATALITIES), totInjur = sum(INJURIES), totPropDam = sum(PROPDMG), totCropDam = sum(CROPDMG))
+  group_by(Year = year(NewStartDate), lookup) %>%
+  summarise(instances = n(), totFat = sum(FATALITIES), totInjur = sum(INJURIES), totPropDam = sum(cumPropDam), totCropDam = sum(cumCropDam))
+yearsEvent <- arrange(yearsEvent, -totFat)
 ####End of Data sumamrization
 
 #The following cod creates different plots
 plot(years$Year, years$totFat, main = "Fatalities by Year, all event Types")
 
-plot(yearsEvent$Year, yearsEvent$totFat, col = yearsEvent$EVTYPE)
-#legend("right", c(1, 2), legend =unique(yearsEvent$EVTYPE), col = unique(yearsEvent$EVTYPE), pch = 2,  )
-
-
+plot(yearsEvent$Year, yearsEvent$totFat, col = as.factor(yearsEvent$lookup))
+#legend("right", c(1, 2), legend =unique(as.factor(yearsEvent$lookup)), col = unique(as.factor(yearsEvent$lookup)), pch = 2,  )
+plot(as.factor(yearsEvent$lookup), yearsEvent$totFat)
 
 ggplot()
 
 #hint: Check REFNUM 605943. PROPDMGEXP is mis-coded. This should be self-evident from the magnitude of the number
-#code that also works
-repdataConcise <- select(repdata, STATE__, STATE, EVTYPE, BGN_DATE, BGN_TIME, END_DATE, END_TIME, 
-                         TIME_ZONE, LENGTH, WIDTH, F, MAG, FATALITIES, INJURIES, PROPDMG, 
-                         PROPDMGEXP, CROPDMG, CROPDMGEXP, WFO)
+
--- newEVETYPEclean.R
+++ newEVETYPEclean.R
@@ -1,12 +1,55 @@
 #clean this better thanever
+#extracts data from table 2.1.something that I pasted into a textfile
+eCodes <- read.table("Eventcodes.txt", sep = "\n", header = TRUE)
 
-read.table("Eventcodes.txt")...etc
+#Gets rid of event type designation ("c, m, z"), everything to lowercase
+eCodes2 <- tolower(sapply(eCodes, function(x) gsub(" [CMZ]$", "", x)))
+eCodes2 <- rbind(eCodes2, "CleanedUpData")
 
-gsub the MCZ values out
+#list of messy data event types, already lowercase
+crapdata <- unique(repdata2$EVTYPE)
+#as result, converts from factors to chr type
 
-get list of unique crapdata
-get list of eventypes
+#Determine which codes can be removed. Keep only prop.crop dmg greater than 1M, Fatalities >0, instances of only 1 or 2
+leastevents <- subset(arrange(events, instances), instances < 3)
+#These are purely exploratory to get the values I chose
+arrange(leastevents, -totPropDam)
+arrange(leastevents, -totPropDam)
+arrange(leastevents, -totFat)
+arrange(leastevents, -instances)
 
-amatch with an lapply gives you the indexes
+#list of EVTYPES to keep from <3 intance occurence
+eveKeep<- unique(rbind(filter(leastevents, totPropDam >= 1000000) %>% select(EVTYPE),
+              filter(leastevents, totCropDam >= 1000000) %>% select(EVTYPE),
+              filter(leastevents, totFat >= 1) %>% select(EVTYPE)
+            )
+)
 
-indext hose against the list for the cleanedtypes
+
+#list of EVTYPES that we can get rid of...
+garbage <- filter(leastevents, (!EVTYPE %in% eveKeep$EVTYPE))
+#list of "Summary" events we can get rid...
+summary <- filter(events, grepl("summary", EVTYPE, fixed = TRUE))
+
+#replaces bad data with placeholder
+crapdata <- case_when(
+      crapdata %in% garbage$EVTYPE ~ "garbage", 
+      crapdata %in% summary$EVTYPE ~ "summary", 
+      TRUE ~ crapdata 
+)
+
+#matches crapdata data codes to 48 oficial codes, then sets them next to eachother
+matchNums <- sapply(crapdata, function(x) if(x !="garbage" && x != "summary"){amatch(x, eCodes2, maxDist = 10)} else {49})
+sum(is.na(eCodes2[matchNums])) #count of NA's remaining
+matchResult <- as.data.frame(cbind(crapdata, lookup = eCodes2[matchNums]), stringsAsFactors = FALSE)
+#count NA's maxDist = 10 = 66 with several errors
+sum(is.na(matchResult))
+
+#Join the new clean codes to the original cleaned dataset
+repdata2 <- left_join(repdata2, matchResult, by = c("EVTYPE" = "crapdata"))
+#remove NA's
+repdata2 <- repdata2[!is.na("lookup"),]
+
+
+# bad indexes
+9, 15, 17, 21, 22, 23, 28
